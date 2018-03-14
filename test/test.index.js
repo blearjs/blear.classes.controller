218aed22e1b60ab35ca9daffdcdcef3b806e7bf1
#云淡然 <cloudcome@163.com>
2018-03-14 17:32:13 +0800

    + 单元测试

--- test/test.index.js
+++ test/test.index.js
@@ -14,7 +14,6 @@ describe('单元测试', function () {
         var ctrl = new Controller();
         var exports = ctrl.export();
 
-        expect(typeof exports.init).toBe('function');
         expect(typeof exports.install).toBe('function');
         expect(typeof exports.show).toBe('function');
         expect(typeof exports.hide).toBe('function');
@@ -28,20 +27,27 @@ describe('单元测试', function () {
         var called1Times = 0;
         var called2Times = 0;
         var calledList = [];
+        var arg = {};
+        var arg1;
+        var arg2;
 
-        ctrl.install(function () {
+        ctrl.install(function (_) {
             called1Times++;
+            arg1 = _;
             calledList.push(0);
         });
 
-        ctrl.install(function () {
+        ctrl.install(function (_) {
+            arg2 = _;
             called2Times++;
             calledList.push(1);
         });
 
         var exports = ctrl.export();
 
-        exports.install();
+        exports.install(arg);
+        expect(arg1).toBe(arg);
+        expect(arg2).toBe(arg);
         expect(called1Times).toBe(1);
         expect(called2Times).toBe(1);
         expect(calledList.length).toBe(2);
@@ -147,22 +153,5 @@ describe('单元测试', function () {
         var exports = ctrl.export();
 
         expect(exports.title).toBe(title);
-    });
-
-    it('#title 控制器导出初始化之后', function () {
-        var ctrl = new Controller();
-        var exports = ctrl.export();
-        var title1 = 'aaa';
-        var title2 = '';
-        var view = {
-            title: function (title) {
-                title2 = title;
-            }
-        };
-        var route = {};
-        exports.init(view, route);
-        ctrl.title(title1);
-
-        expect(title2).toEqual(title1);
     });
 });
